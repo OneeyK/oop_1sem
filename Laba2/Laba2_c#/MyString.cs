c1c67cee4e0740f21a93b5f4f0b49f3e9cef7037
OneeyK <54153791+OneeyK@users.noreply.github.com>
2020-04-01 15:49:02 +0300

    Create MyString.cs

---
+++ Laba2/Laba2_c#/MyString.cs
@@ -0,0 +1,34 @@
+Юра, [01.04.20 15:15]
+using System;
+namespace laba2_oop
+{
+    public class MyString
+    {
+        char[] Str;
+
+        public MyString(string str)
+        {
+            Str = str.ToCharArray();
+        }
+
+        public char[] ToChar
+        {
+            get
+            {
+                return Str;
+            }
+        }
+
+        public int GetLength()
+        {
+            return Str.Length;
+        }
+
+        public void UppLetters()
+        {
+            for (int i = 0; i < Str.Length; i++)
+                if (char.IsLetter(Str[i]))
+                    Str[i] = char.ToUpper(Str[i]);
+        }
+    }
+}
