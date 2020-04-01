cfab3f3f61c75a1fd2211892097c215c9c0efe15
OneeyK <54153791+OneeyK@users.noreply.github.com>
2020-04-01 15:52:59 +0300

    Create Program.cs

---
+++ Laba3/Program.cs
@@ -0,0 +1,14 @@
+using System; 
+ 
+namespace Laba3 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            MyArray a = new MyArray(5, 5); 
+            int d = a[3]; 
+            int b = a.GetAverage; 
+        } 
+    } 
+}
